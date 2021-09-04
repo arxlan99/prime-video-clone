6637596f26733c506c03eb8e480cd9bf2aa09413
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-09-04 22:21:40 +0300

    Build navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { ArrowDropDown, Search } from "@material-ui/icons";
 import React from "react";
 import classes from "./Navbar.module.scss";
 
@@ -6,15 +7,35 @@ const Navbar = () => {
     <div className={classes.navbar}>
       <div className={classes.container}>
         <div className={classes.left}>
-          <img src={process.env.PUBLIC_URL + "/images/logo.png"} alt="" />
+          <img src={process.env.PUBLIC_URL + "/images/logo1.png"} alt="logo" />
           <span>Home</span>
           <span>Tv Shows</span>
           <span>Movies</span>
           <span>Kids</span>
         </div>
         <div className={classes.right}>
-          <span>Search</span>
-          <span>Username</span>
+          <Search className={classes.search} />
+          <img src={process.env.PUBLIC_URL + "/images/user.png"} alt="user" />
+          <span className={classes.icon}>Username</span>
+
+          <div className={classes.profile}>
+            <ArrowDropDown className={classes.icon} />
+            <div className={classes.options}>
+              <div className={classes.options_left}>
+                <span>Your Watchlist</span>
+                <span>Account and Setting</span>
+                <span>Watch Anywhere </span>
+                <span>Help</span>
+                <span className={classes.options_left__signout}>Sign Out</span>
+              </div>
+              <div className={classes.options_right}>
+                <span>Kids</span>
+                <span>Add new</span>
+                <span>Manage Profiles </span>
+                <span>Learn More</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
